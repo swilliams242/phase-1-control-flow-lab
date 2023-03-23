6e311313ef809772d83e4302f3ebaf7361ef3506
Selena Williams <s.a.williams242@gmail.com>
2023-03-22 20:03:00 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  } else if (someValue >= 400 && someValue <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (someValue > 2000 && someValue < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (someValue > 2500) {
+    result = "No can do.";
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  let result
+  if (someCity === 'NYC') {
+    result = "Ok, sounds good.";
+  } else {
+    result = "No go.";
+  }
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result
+  if (tip === 'generous') {
+    result = "Thank you so much.";
+  } else if (tip === 'not as generous') {
+    result = "Thank you.";
+  } else {
+    result = "Bye.";
+  }
+  return result
 }
